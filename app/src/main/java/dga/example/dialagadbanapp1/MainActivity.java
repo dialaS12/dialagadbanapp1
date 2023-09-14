d8717f251306b9828e4a22c44067fb76d2391cdc
diala <dialagadban5@gmail.com>
2023-09-14 15:00:37 +0300

    اكمل الاشياء

--- app/src/main/java/dga/example/dialagadbanapp1/MainActivity.java
+++ app/src/main/java/dga/example/dialagadbanapp1/MainActivity.java
@@ -3,6 +3,8 @@ package dga.example.dialagadbanapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("fi","on create");
+        Toast.makeText(this, "on create", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("fi","on Restart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("fi","onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+
     }
 }
