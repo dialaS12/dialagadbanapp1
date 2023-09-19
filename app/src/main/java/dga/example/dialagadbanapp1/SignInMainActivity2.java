82ef96c3a3d57174814b2c87708bd495387b96a5
diala <dialagadban5@gmail.com>
2023-09-19 14:59:32 +0300

    ديالا الرا~عه

---
+++ app/src/main/java/dga/example/dialagadbanapp1/SignInMainActivity2.java
@@ -0,0 +1,14 @@
+package dga.example.dialagadbanapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInMainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_main2);
+    }
+}
