6faeff8f422c5474037252554cfdb2385183e7d0
diala <dialagadban5@gmail.com>
2023-10-03 14:55:10 +0300

    ديالا الرا~عه

--- app/src/main/java/dga/example/dialagadbanapp1/data/AppDatabase.java
+++ app/src/main/java/dga/example/dialagadbanapp1/data/AppDatabase.java
@@ -2,12 +2,17 @@ package dga.example.dialagadbanapp1.data;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import dga.example.dialagadbanapp1.data.mytaskTable.MyTask;
 import dga.example.dialagadbanapp1.data.mytaskTable.myTaskQuery1;
+import dga.example.dialagadbanapp1.data.subjectTable.MySubject;
 import dga.example.dialagadbanapp1.data.subjectTable.MySubjectQuery1;
+import dga.example.dialagadbanapp1.data.usersTable.MyUser;
 import dga.example.dialagadbanapp1.data.usersTable.MyUserQuery;
+@Database(entities = {MyUser.class, MySubject.class, MyTask.class},version = 5)
 
 public abstract class AppDatabase extends RoomDatabase
 
