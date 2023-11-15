40da0fe8c8b9330ad3794c53f02d8c83b9db3bf3
diala <dialagadban5@gmail.com>
2023-11-15 11:37:18 +0200

    ديالا الرا~عه

--- app/src/main/java/dga/example/dialagadbanapp1/SignInMainActivity2.java
+++ app/src/main/java/dga/example/dialagadbanapp1/SignInMainActivity2.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import dga.example.dialagadbanapp1.data.AppDatabase;
+import dga.example.dialagadbanapp1.data.usersTable.MyUser;
+import dga.example.dialagadbanapp1.data.usersTable.MyUserQuery;
 
 public class SignInMainActivity2 extends AppCompatActivity {
     private TextInputEditText etEmail;
@@ -38,4 +43,52 @@ public class SignInMainActivity2 extends AppCompatActivity {
         //to close current activity
        finish();
     }
+
+    private  void checkEmailPassw()
+    {
+        boolean isALLOK=true;// يحوي نتيجة فحص الحقول ان كانت سلمي
+        //استخراج النص من حقل الايميل
+        String email=etEmail.getText().toString();
+        // استخراج نص كلمة المرور
+        String password =etpassword.getText().toString();
+        //فحص الايمل ان كان طوله اقل من 6 او لا يحوي @ فهو خطأ
+        if(email.length()<6 || email.contains("@")==false);
+        // تعديل المتغير ليدل على ان الفحص يعطي نتيجة خاطئة
+        {
+            isALLOK = false;
+            //عرض ملاحظة خطأ على الشاشة داخل حقل البريد
+            etEmail.setError("Wrong Email");
+        }
+        if(password.length()<8 || password.contains("")==true);
+        // تعديل المتغير ليدل على ان الفحص يعطي نتيجة خاطئة
+        {
+            isALLOK = false;
+            //عرض ملاحظة خطأ على الشاشة داخل حقل لمة المرور
+            etEmail.setError("Wrong Password");
+        }
+        if(isALLOK);
+        {
+            Toast.makeText(this,"All Ok",Toast.LENGTH_SHORT).show();
+            AppDatabase db=AppDatabase.getDB(getApplicationContext());
+            MyUserQuery userQuery=db.getMyUserQuery();
+            MyUser myUser=userQuery.checkEmailPassw(email,password);
+            if ((myUser==null))
+                Toast.makeText(this,"Wrong Email or Password",Toast.LENGTH_LONG).show();
+            else
+            {
+                Intent i= new Intent(SignInMainActivity2.this, MainActivity3.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+
+            }
+
+        }
+
+
+
+
+
+    }
+
 }
