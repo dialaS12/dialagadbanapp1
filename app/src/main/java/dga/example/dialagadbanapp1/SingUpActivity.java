5f882a2d0869a2fd3b79a86265e8373a32281fa4
diala <dialagadban5@gmail.com>
2023-11-15 11:37:18 +0200

    ديالا الرا~عه

--- app/src/main/java/dga/example/dialagadbanapp1/SingUpActivity.java
+++ app/src/main/java/dga/example/dialagadbanapp1/SingUpActivity.java
@@ -4,10 +4,14 @@ import  androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import dga.example.dialagadbanapp1.data.AppDatabase;
+import dga.example.dialagadbanapp1.data.usersTable.MyUser;
+import dga.example.dialagadbanapp1.data.usersTable.MyUserQuery;
 
 public class SingUpActivity extends AppCompatActivity
 {
@@ -30,13 +34,59 @@ public class SingUpActivity extends AppCompatActivity
         etName= (TextInputEditText) findViewById(R.id.etName);
         etPhone= (TextInputEditText) findViewById(R.id.etPhone);
         btnSave= (Button) findViewById(R.id.btnSave);
+
     }
-    public void onClickMain(View v)
-    {
-        //to open new activity from current to next
-        Intent i= new Intent(SingUpActivity.this,MainActivity3.class);
-        startActivity(i);
-        //to close current activity
-        finish();
+    public void checkEPRP() {
+        boolean isAllOk = true;
+        String name=etName.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etpassword.getText().toString();
+        String repassword = etRePassword.getText().toString();
+        String phone=etPhone.getText().toString();
+
+        if (email.length() < 6 || email.contains("@") == false) ;
+        {
+            isAllOk = false;
+            etEmail.setError("Wrong Email");
+        }
+        if (password.length() < 8 || password.contains("") == true)
+        {
+            isAllOk = false;
+            etpassword.setError("Wrong Password");
+        }
+        if (repassword.length() ==password.length() || repassword.contains(password) == true)
+        {
+            isAllOk=false;
+            etRePassword.setError("Wrong RePassword");
+        }
+
+        if (phone.length()<10||phone.length()>10||phone.length()!=10||phone.contains("")==true)
+        {
+            isAllOk=false;
+            etPhone.setError("Wrong Phone");
+        }
+        if (isAllOk) {
+            AppDatabase db=AppDatabase.getDB(getApplicationContext());
+            MyUserQuery userQuery=db.getMyUserQuery();
+            if (userQuery.checkEmailPassw(email!=null))
+            {
+                etEmail.setError("found email");
+            }
+            else  {
+                MyUser myUser=new MyUser();
+                myUser.fullName=name;
+                myUser.phone=phone;
+                myUser.passw=password;
+                myUser.email=email;
+                userQuery.insert((myUser));
+                finish();
+
+
+
+            }
+
+        }
+
     }
 }
+
