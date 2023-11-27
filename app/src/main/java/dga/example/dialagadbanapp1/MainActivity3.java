6f9cf479110398e3f316b0459e572366a5429fd0
diala <dialagadban5@gmail.com>
2023-11-27 14:11:56 +0200

    1. menu 2.com

--- app/src/main/java/dga/example/dialagadbanapp1/MainActivity3.java
+++ app/src/main/java/dga/example/dialagadbanapp1/MainActivity3.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.List;
+
+import dga.example.dialagadbanapp1.data.AppDatabase;
+import dga.example.dialagadbanapp1.data.mytaskTable.MyTask;
+import dga.example.dialagadbanapp1.data.subjectTable.MySubject;
+import dga.example.dialagadbanapp1.data.subjectTable.MySubjectQuery1;
 
 public class MainActivity3 extends AppCompatActivity {
     //spnr1 تعريف صفه للكائن المرئي
     private Spinner spnrSubject;
+    private ListView istvTasks;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        istvTasks= (ListView) findViewById(R.id.istvTasks);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         //spnr2 وضع مؤشر الصفه على الكائن المرئي الموجود بواجهه المستعمل
@@ -25,6 +37,19 @@ public class MainActivity3 extends AppCompatActivity {
         adapter.addAll(ar);
         //spnr6ربط الكائن المرئي بالوسيط
         spnrSubject.setAdapter(adapter);
+
     }
+    private void initSubjectSpnr()
+    {
+        AppDatabase db=AppDatabase.getDB(getApplicationContext());//قاعدة بناء
+
+        List<MyTask>allSubject=allTasks.getAll();//استخراج جميع المواضيع
+
+        ArrayAdapter<String>tasktAdapter=new ArrayAdapter<MyTask>(this,android.R.layout.simple_list_item_1);
+        tasktAdapter.add(allTasks);
+        lstTasks.setAdapter(tasktAdapter);
+    }
+
+
 
     }
