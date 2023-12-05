d7cde6a2af739745077dbf2f6255ae3dc5d168b6
diala <dialagadban5@gmail.com>
2023-12-05 20:41:21 +0200

    idfh

--- app/src/main/java/dga/example/dialagadbanapp1/MainActivity3.java
+++ app/src/main/java/dga/example/dialagadbanapp1/MainActivity3.java
@@ -1,5 +1,6 @@
 package dga.example.dialagadbanapp1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,9 +15,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.textfield.TextInputEditText;
 
-import java.security.Key;
 import java.util.List;
 
 import dga.example.dialagadbanapp1.data.AppDatabase;
@@ -30,49 +29,48 @@ public class MainActivity3 extends AppCompatActivity {
     private Spinner spnrSubject;
     private ListView istvTasks;
     private FloatingActionButton fabAdd;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        istvTasks=  findViewById(R.id.istvTasks);
-        spnrSubject= (Spinner) findViewById(R.id.spnrSubject);
-        fabAdd= (FloatingActionButton) findViewById(R.id.fabAdd);
+        istvTasks = findViewById(R.id.istvTasks);
+        spnrSubject = (Spinner) findViewById(R.id.spnrSubject);
+        fabAdd = (FloatingActionButton) findViewById(R.id.fabAdd);
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent i= new Intent(MainActivity3.this, AddTaskActivity1.class);
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity3.this, AddTaskActivity1.class);
                 startActivity(i);
 
             }
         });
 
         //spnr2 وضع مؤشر الصفه على الكائن المرئي الموجود بواجهه المستعمل
-        spnrSubject=findViewById(R.id.spnrSubject);
+        spnrSubject = findViewById(R.id.spnrSubject);
         //spnr3 بناء الوسيط وتحديد واجهه تنسيق لمعطى واحد
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //spnr4 data sourceمصدر معطيات (ممكن ان يكون قائمة من قاعدة بيانات مثلا)
-        String[]ar={"Math","CS","Phs","Arb","Eng"};
+        String[] ar = {"Math", "CS", "Phs", "Arb", "Eng"};
         //spnr5تحديد المعطيات للوسيط
         adapter.addAll(ar);
         //spnr6ربط الكائن المرئي بالوسيط
         spnrSubject.setAdapter(adapter);
 
     }
-    private void initSubjectSpnr()
-    {
 
-        AppDatabase db=AppDatabase.getDB(getApplicationContext());// قاعدة بناء
-        MySubjectQuery1 subjectQuery=db.getMySubjectQuery();//  عمليات جدول المواضيع
-        List<MySubject> allSubjects=subjectQuery.getAll();// استخراج// جميع المواضيع
+    private void initSubjectSpnr() {
+
+        AppDatabase db = AppDatabase.getDB(getApplicationContext());// قاعدة بناء
+        MySubjectQuery1 subjectQuery = db.getMySubjectQuery();//  عمليات جدول المواضيع
+        List<MySubject> allSubjects = subjectQuery.getAll();// استخراج// جميع المواضيع
         // قاعدة بناء
-        ArrayAdapter <String> SubjectAdapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line);
+        ArrayAdapter<String> SubjectAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line);
 
         SubjectAdapter.add("ALL");//ستظهر اولا بالسبنر تعني عرض جميع المهمات
-        for (MySubject subject:allSubjects)//  اضافة المواضبع للوسيط
+        for (MySubject subject : allSubjects)//  اضافة المواضبع للوسيط
         {
             SubjectAdapter.addAll(subject.title);
         }
@@ -104,58 +102,66 @@ public class MainActivity3 extends AppCompatActivity {
     }
 
 
-            private void initAllListView() {
+    private void initAllListView() {
 
-                AppDatabase db = AppDatabase.getDB(getApplicationContext());// قاعدة بناء
+        AppDatabase db = AppDatabase.getDB(getApplicationContext());// قاعدة بناء
 
-                MyTaskQuery1 taskQuery1 = db.getMyTaskQuery();
-                List<MyTask> allTasks = taskQuery1.getAllTasks();
-                ArrayAdapter<MyTask> TaskAdapter = new ArrayAdapter<MyTask>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line);
+        MyTaskQuery1 taskQuery1 = db.getMyTaskQuery();
+        List<MyTask> allTasks = taskQuery1.getAllTasks();
+        ArrayAdapter<MyTask> TaskAdapter = new ArrayAdapter<MyTask>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line);
 
-                TaskAdapter.addAll(allTasks);
-                istvTasks.setAdapter(TaskAdapter);
-
+        TaskAdapter.addAll(allTasks);
+        istvTasks.setAdapter(TaskAdapter);
+        istvTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showPopUpMenu(view, TaskAdapter.getItem(i));//!!
             }
+        });
 
-            /**
-             * تجهيز قا~مة المهمات حسب رقم الموضوع
-             * رقم الموضوع param key_id
-             */
-            private void initListViewBySubjId(long key_id) {
-                AppDatabase db = AppDatabase.getDB(getApplicationContext());
-                MyTaskQuery1 taskQuery1 = db.getMyTaskQuery();
-                // يجب اضافة عملية تعيد جميع المهمات حسب رقم الموضوغ
-                List<MyTask> allTasks = taskQuery1.getTasksBySubjId(key_id);
-                ArrayAdapter<MyTask> TaskAdapter = new ArrayAdapter<MyTask>(this, android.R.layout.simple_list_item_1);
-                TaskAdapter.addAll(allTasks);
-                istvTasks.setAdapter(TaskAdapter);
+    }
 
-            }
+    /**
+     * تجهيز قا~مة المهمات حسب رقم الموضوع
+     * رقم الموضوع param key_id
+     */
+    private void initListViewBySubjId(long key_id) {
+        AppDatabase db = AppDatabase.getDB(getApplicationContext());
+        MyTaskQuery1 taskQuery1 = db.getMyTaskQuery();
+        // يجب اضافة عملية تعيد جميع المهمات حسب رقم الموضوغ
+        List<MyTask> allTasks = taskQuery1.getTasksBySubjId(key_id);
+        ArrayAdapter<MyTask> TaskAdapter = new ArrayAdapter<MyTask>(this, android.R.layout.simple_list_item_1);
+        TaskAdapter.addAll(allTasks);
+        istvTasks.setAdapter(TaskAdapter);
+
+    }
+
     /**
      * داله مساعدة لفنح قائمة تتلقى بارمتر للكائن الذي سبب فتح القائمة
+     *
      * @param v
      */
     public void showPopUpMenu(View v) {
         // بناء قائمة popup menu
         PopupMenu popup = new PopupMenu(this, v);//visilel|
-        popup.inflate (R.menu.pupupmenu);
+        popup.inflate(R.menu.pupupmenu);
         popup.show();
 
     }
-    public  void onClick(View v)
-    {
+
+    public void onClick(View v) {
         showPopUpMenu(v);
     }
 
     /**
      * داله مساعدة لفتح قائمة تتلقى بارمتر للكائن الذي سبب فتح القائمة
+     *
      * @param v
      * @param item
      */
-    public void showPopUpMenu(View v,MyTask item)
-    {
+    public void showPopUpMenu(View v, MyTask item) {
         //بناء القائمة popup menu
-        PopupMenu popup=new PopupMenu(this,v);//لكائن الذي سبب فتح القائمة v
+        PopupMenu popup = new PopupMenu(this, v);//لكائن الذي سبب فتح القائمة v
         //
         popup.inflate(R.menu.pupupmenu);
         //
@@ -163,8 +169,8 @@ public class MainActivity3 extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
 
-                if (menuItem.getItemId() == R.id.mnAddTask) {
-                    Toast.makeText(MainActivity3.this, "Add", Toast.LENGTH_SHORT).show();
+                if (menuItem.getItemId() == R.id.mnComplete) {
+                    Toast.makeText(MainActivity3.this, "Complete", Toast.LENGTH_SHORT).show();
                     Intent i = new Intent(MainActivity3.this, AddTaskActivity1.class);
                     startActivity(i);
                 }
@@ -179,16 +185,46 @@ public class MainActivity3 extends AppCompatActivity {
             }
 
 
-
-
         });
         popup.show();
     }
-   // public boolean onMenultemClick(MenuItem menultem)
-   {
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId()==R.id.itemSettings)
+        {
+            Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
+            //to open new activity from current to next activity
+            Intent i = new Intent(MainActivity3.this, AddTaskActivity1.class);
+            startActivity(i);
+        }
+        if (item.getItemId()==R.id.itemSignOut) {
+            Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
+
+        }
+        if (item.getItemId()==R.id.itemAddTask)
+        {
+            Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
+            //to open new activity from current to next activity
+            Intent i = new Intent(MainActivity3.this, AddTaskActivity1.class);
+            startActivity(i);
+        }
+        }
 
 
-   }}
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
 
 
 
